3833b8a7df67609afc434933fe1851a95e1bef0b
Lucas Pugens Fernandes <lpugens@gmail.com>
2022-09-18 15:09:33 -0300

    test: readding table

--- LPugensWebCDK/DBStack.py
+++ LPugensWebCDK/DBStack.py
@@ -8,12 +8,12 @@ class DBStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # self.table = dynamo.Table(
-        #     self,
-        #     'test_db',
-        #     partition_key=dynamo.Attribute(
-        #         name='id',
-        #         type=dynamo.AttributeType.STRING,
-        #     ),
-        #     billing_mode=dynamo.BillingMode.PAY_PER_REQUEST,
-        # )
+        self.table = dynamo.Table(
+            self,
+            'test_db',
+            partition_key=dynamo.Attribute(
+                name='id',
+                type=dynamo.AttributeType.STRING,
+            ),
+            billing_mode=dynamo.BillingMode.PAY_PER_REQUEST,
+        )
