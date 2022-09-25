5239e39a55ea545b19c79ffbc9eebacf024568fa
Lucas Pugens Fernandes <lpugens@gmail.com>
2022-09-25 15:18:42 -0300

    Adding necessary private endpoints

--- LPugensWebCDK/NetworkStack.py
+++ LPugensWebCDK/NetworkStack.py
@@ -20,12 +20,50 @@ class NetworkStack(Stack):
             enable_dns_hostnames=True,
             enable_dns_support=True
         )
-        
+        # Add VPC endpoints to keep the traffic inside AWS
+        self.s3_private_link = ec2.GatewayVpcEndpoint(
+            self,
+            "S3GWEndpoint",
+            vpc=self.vpc,
+            service=ec2.GatewayVpcEndpointAwsService.S3
+        )
+        self.ecr_api_private_link = ec2.InterfaceVpcEndpoint(
+            self,
+            "ECRapiEndpoint",
+            vpc=self.vpc,
+            service=ec2.InterfaceVpcEndpointAwsService.ECR,
+            open=True,
+            private_dns_enabled=True
+        )
+        self.ecr_dkr_private_link = ec2.InterfaceVpcEndpoint(
+            self,
+            "ECRdkrEndpoint",
+            vpc=self.vpc,
+            service=ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER,
+            open=True,
+            private_dns_enabled=True
+        )
         self.cloudwatch_private_link = ec2.InterfaceVpcEndpoint(
             self,
             "CloudWatchEndpoint",
             vpc=self.vpc,
             service=ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
+            open=True,
+            private_dns_enabled=True
+        )
+        self.secrets_manager_private_link = ec2.InterfaceVpcEndpoint(
+            self,
+            "SecretsManagerEndpoint",
+            vpc=self.vpc,
+            service=ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
+            open=True,
+            private_dns_enabled=True
+        )
+        self.sqs_private_link = ec2.InterfaceVpcEndpoint(
+            self,
+            "SQSEndpoint",
+            vpc=self.vpc,
+            service=ec2.InterfaceVpcEndpointAwsService.SQS,
             open=True,
             private_dns_enabled=True
         )
